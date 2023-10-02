520f7b61be04b9647127977cb32135390546a21e
Thiago <thiagoapalacios@gmail.com>
2023-10-01 23:44:43 -0300

    fix: Correcting video display on the Class page

--- src/pages/Class/styles.ts
+++ src/pages/Class/styles.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components/native";
+import theme from "../../theme/themes";
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  padding: 12px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.SECONDARY_BASE_02};
+  border-radius: 8px;
+  margin-top: 100px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${theme.colors.LIGHT_BASE_01};
+  font-family: ${theme.FONT_WEIGHT.Barlow_600};
+  font-size: 16px;
 `;
--- src/pages/Class/view.tsx
+++ src/pages/Class/view.tsx
@@ -1,26 +1,89 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, ButtonText, Container } from "./styles";
 import { PropsView } from "./Models";
-import VideoPlayer from "expo-video-player";
-import { ResizeMode } from "expo-av";
+import { Video, ResizeMode } from "expo-av";
 import { Screen } from "../../components";
+import { StyleSheet, View } from "react-native";
 
 const ClassView: React.FC<PropsView> = ({ src, title, navigation }) => {
+  const video: any = useRef(null);
+  const [status, setStatus] = useState({});
+
+  //Focus Event: to be fired when the HomeScreen is focused.
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      console.log("Focus");
+      //Every time the screen is focused the Video starts playing
+      if (video) {
+        video.current.playAsync();
+      }
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  //Blur Event: to be fired when the HomeScreen loses focus.
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("blur", () => {
+      console.log("Blur");
+      //Every time the screen loses focus the Video is paused
+      if (video) {
+        video.current.pauseAsync();
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <Screen backbutton backPress={() => navigation.goBack()} title={title}>
+    <Screen
+      backbutton
+      backPress={() => {
+        navigation.goBack();
+        video.current.pauseAsync();
+      }}
+      title={title}
+    >
       <Container>
-        <VideoPlayer
-          videoProps={{
-            shouldPlay: true,
-            resizeMode: ResizeMode.CONTAIN,
-            source: {
-              uri: src,
-            },
+        <Video
+          ref={video}
+          style={styles.video}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          source={{
+            uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
           }}
+          useNativeControls={true}
+          shouldPlay={true}
+          resizeMode={ResizeMode.CONTAIN}
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
         />
+
+        <Button
+          onPress={() =>
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync()
+          }
+        >
+          <ButtonText>{status.isPlaying ? "Pause" : "Play"}</ButtonText>
+        </Button>
       </Container>
     </Screen>
   );
 };
 
 export default ClassView;
+
+const styles = StyleSheet.create({
+  video: {
+    alignSelf: "center",
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
